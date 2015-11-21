08a7106fb7d8b31229896c059c0aa4374c8dfd1f
heaguado <heaguado@gmail.com>
2015-11-21 12:15:46 +0100

    adding public part

--- config/env/index.js
+++ config/env/index.js
@@ -1,5 +1,6 @@
 var env = process.env.NODE_ENV;
 
-module.exports = (env == "dev" ) ?  require('./development') :
-				 (env == "prod") ?  require('./production')  :
+module.exports = (env == 'dev' ) ?  require('./development') :
+				 (env == 'prod') ?  require('./production')  :
+				 (env == 'test') ?  require('./test')  :
 									require('./local');
