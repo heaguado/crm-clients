30ecfbb5eda720f1f8dad366eacfbe3d6b4d3fa9
heaguado <heaguado@gmail.com>
2016-01-07 21:42:05 +0100

    integration test and register user

--- config/env/production.js
+++ config/env/production.js
@@ -3,12 +3,16 @@
 module.exports = {
     environment: process.env.NODE_ENV || "prod",
     server: {
-        port: process.env.PORT || 7443
+      port: process.env.PORT || 42175
     },
     mongo: {
-        hosts: process.env.MONGO_HOSTS || '',
-        db: process.env.MONGO_DB || 'crmclients',
-        options: {}
+      hosts: process.env.MONGO_HOSTS || 'heroku_69w913dx:q500jvei47on8858toau2puovb@ds039155.mongolab.com:39155',
+      db: process.env.MONGO_DB || 'heroku_69w913dx',
+      options: {},
+      auth: {
+        user: 'heaguado',
+        pass: 'haguado22'
+      }
     },
     secretKey: 'figuetuffueñof'
 };
